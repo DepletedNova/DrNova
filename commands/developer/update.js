714f191fc659e84a8c9dda6a8596a46605d6f824
Zoey D <zdavis8529@gmail.com>
2022-06-25 12:14:25 -0600

    Update update.js

--- commands/developer/update.js
+++ commands/developer/update.js
@@ -7,17 +7,17 @@ module.exports = {
     devOnly: true,
     run: async ({bot, message, args}) => {
         const {client} = bot
-        var dat = new Date()
-        await client.gitUpdater.forceUpdate()
-        await client.loadEvents(bot, true)
-        await client.loadCommands(bot, true)
-        await client.loadResponses(bot, true)
-        await client.loadButtons(bot, true)
-        await client.Data.reload(bot)
-        //delete require.cache[require.resolve('.../handlers/credit')]
-        //client.handleCredit = (bot, msg, amount) => require(".../handlers/credit")(bot, msg, amount)
-        console.log(`Update took ${Math.abs(Date.now() - dat)}ms`)
-        message.reply(`Updated bot in ${Math.abs(Date.now() - dat)}ms`).then
-        delete dat
+        const channel = message.channel
+        const dat = new Date()
+        client.gitUpdater.forceUpdate().then(() => {
+            await client.loadEvents(bot, true)
+            await client.loadCommands(bot, true)
+            await client.loadResponses(bot, true)
+            await client.loadButtons(bot, true)
+            await client.Data.reload(bot)
+            console.log(`Update took ${Math.abs(Date.now() - dat)}ms`)
+            message.send(`Updated bot in \`${Math.abs(Date.now() - dat)/1000}s\``)
+            delete dat
+        })
     }
 }
