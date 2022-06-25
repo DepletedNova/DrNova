6ab34c37a063b6e207e88544a9d5dfffcd003e10
Zoey D <zdavis8529@gmail.com>
2022-06-25 12:00:51 -0600

    Initial

--- commands/developer/update.js
+++ commands/developer/update.js
@@ -1,13 +1,23 @@
 module.exports = {
     name: "update",
     category: "developer",
-    description: "Updates the bot based on Github",
+    description: "Properly updates the bot from Github",
     usage: `dr.update`,
     permissions: [],
     devOnly: true,
     run: async ({bot, message, args}) => {
         const {client} = bot
-        
-        client.gitUpdater.forceUpdate()
+        var dat = new Date()
+        await client.gitUpdater.forceUpdate()
+        await client.loadEvents(bot, true)
+        await client.loadCommands(bot, true)
+        await client.loadResponses(bot, true)
+        await client.loadButtons(bot, true)
+        await client.Data.reload(bot)
+        //delete require.cache[require.resolve('.../handlers/credit')]
+        //client.handleCredit = (bot, msg, amount) => require(".../handlers/credit")(bot, msg, amount)
+        console.log(`Update took ${Math.abs(Date.now() - dat)}ms`)
+        message.reply(`Updated bot in ${Math.abs(Date.now() - dat)}ms`).then
+        delete dat
     }
 }
